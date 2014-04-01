7db0144fbeae2bd07676f67b0d4f14df94e3e1c9
ktysktest1 <krystian.tyskiewicz1@o2.pl>
2014-04-01 20:50:45 +0200

    Second user commit test

--- Test.php
+++ Test.php
@@ -1,16 +1,17 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of Test
- *
- * @author Krystian
- */
-class Test {
-    //put your code here
+class Test
+{
+    /**
+     * test
+     * 
+     * @return array
+     */
+    public function test()
+    {
+        return array(
+            'test' => 'test2',
+            'other' => true
+        );
+    }
 }
